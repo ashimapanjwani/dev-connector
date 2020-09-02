292bc297e59c88ff522d1753da136eb96d0f6d6b
33724707 <ashimapanjwani@gmail.com>
2020-09-02 19:14:55 +0530

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route    POST api/users
 // @desc     Register user
@@ -14,13 +17,48 @@ router.post(
       .trim()
       .isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('Users route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({
+            errors: [
+              {
+                value: email,
+                msg: 'User already exists',
+                param: 'email',
+                location: 'body',
+              },
+            ],
+          });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User registered');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
