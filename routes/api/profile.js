17b5e46e4b079b5c73517200c057b6d21f815acc
33724707 <ashimapanjwani@gmail.com>
2020-09-15 22:21:16 +0530

    delete user's posts during account deletion

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,7 @@ const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route    GET api/profile/me
 // @desc     Get current user's profile
@@ -158,6 +159,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access   Private
 router.delete('/', auth, async (req, res) => {
   try {
+    await Post.deleteMany({ user: req.user.id });
+
     await Profile.findOneAndRemove({
       user: req.user.id,
     });
